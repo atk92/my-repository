28aa270e22cae1518e9036c874be71f42b8b6d8c
note8promtk9@gmail.com <python31@996931658>
2022-05-16 14:45:06 -0400

    calculadora
    funcinando

--- calculadora/calc.js
+++ calculadora/calc.js
@@ -1,8 +1,23 @@
-function insert (num) {
+function inserirNum (num) {
    var numero = document.getElementById('resultado').innerHTML;
    document.getElementById('resultado').innerHTML = numero + num;
 }
 
 function limpar() {
     document.getElementById('resultado').innerHTML = " ";
+}
+
+function apagadorNum() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+function calcular () {
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else {
+        document.getElementById('resultado').innerHTML = "NaN Calc"
+    }
+
 }
